084da80b776d7dfa74d548c9ee755a67d95397b2
Khosi Morafo <khosimorafo@yahoo.com>
2019-07-02 13:14:27 +0200

    first commit

--- install/create.sh
+++ install/create.sh
@@ -2,6 +2,12 @@
 
 sed -e "s;%WORKING_DIR%;$1;g" -e "s;%YARN%;$2;g" -e "s;%DIR%;$3;g" service.template > xrplkd.service
 
+cd $1
+
+sudo $2 build 
+
+cd $1/install
+
 sudo cp xrplkd.service /etc/systemd/system
 
 sudo rm xrplkd.service
@@ -9,4 +15,3 @@ sudo rm xrplkd.service
 sudo systemctl enable xrplkd.service 
 
 sudo systemctl start xrplkd.service
-
